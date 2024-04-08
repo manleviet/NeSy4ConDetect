37f6f82e61b19e3f1e40425f185c3f24a9f02e9f
Lê Viết Mẫn <manleviet@gmail.com>
2024-04-09 01:12:49 +0200

    changed: key is feature name, value is index

--- utils/index/create_index_file.py
+++ utils/index/create_index_file.py
@@ -13,9 +13,9 @@ for line in lines:
         # split the line into elements
         elements = line.strip().split()
         if len(elements) >= 3:
-            # second element is the index of the feature
-            # third element is the name of the feature
-            feature_map[int(elements[1])] = elements[2]
+            # key - third element, the name of the feature
+            # value -second element, the index of the feature
+            feature_map[elements[2]] = int(elements[1])
 
 # print the feature_map dictionary
 for key, value in feature_map.items():
